200631617ad0d1026a7d0d5b3c363d4d06da7390
Едиге Нуркайыр Маратулы <@corp.itstep>
2018-05-29 11:44:33 +0600

    without Asynch

---
+++ FileClient/FileClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileClient
+{
+    class Program
+    {
+        private static int defaultPort = 3535;
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(IPAddress.Parse("127.0.0.1"), defaultPort);
+            try
+            {
+                Console.WriteLine("Клиент запущен...");
+
+                try
+                {
+                    string fileName = "2.zip";
+                    string filePath = @"C:\Users\едиген\Downloads\";
+
+                    byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+                    byte[] fileData = File.ReadAllBytes(filePath + fileName);
+                    byte[] clientData = new byte[4 + fileNameByte.Length + fileData.Length];
+                    byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
+
+                    fileNameLen.CopyTo(clientData, 0);
+                    fileNameByte.CopyTo(clientData, 4);
+                    fileData.CopyTo(clientData, 4 + fileNameByte.Length);
+
+                    client.Client.Send(clientData);
+
+                    Console.WriteLine("Файл отправлен: {0}",fileName);
+                    client.Client.Close();
+                    Console.ReadLine();
+                    //client.Client.SendFile(path);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Загрузка не удалась, {0}", ex.Message);
+                }
+                Console.ReadLine();
+                client.Close();
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine("Error: {0}",ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+            Console.WriteLine("Завершение работы...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FileServer/FileServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileServer
+{
+    class Program
+    {
+        private static int defaultPort = 3535;
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), defaultPort);
+            try
+            {
+                server.Start();
+                Console.WriteLine("Сервер запущен. Ждет подключений...");
+                TcpClient client = server.AcceptTcpClient();
+
+                Console.WriteLine("Входящее соединение...");
+                byte[] clientData = new byte[1024 * 5000];
+                string receivedPath = "d:/";
+
+                int receivedBytesLen = client.Client.Receive(clientData);
+                int fileNameLen = BitConverter.ToInt32(clientData, 0);
+                string fileName = Encoding.ASCII.GetString(clientData, 4, fileNameLen);
+
+                BinaryWriter bWrite = new BinaryWriter(File.Open(receivedPath + fileName, FileMode.Append)); ;
+                bWrite.Write(clientData, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
+
+                Console.WriteLine("Файл: {0} получен & сохранен: {1}", fileName, receivedPath);
+                bWrite.Close();
+                client.Close();
+                Console.ReadLine();
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine("Error {0}" ,ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
